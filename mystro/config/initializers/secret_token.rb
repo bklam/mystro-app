cd5c68d4d2a1266089048a7baf277905ce6e9660
Ben Lam <bklam@andrew.cmu.edu>
2014-12-23 00:59:27 -0500

    generated new mystro rails app

---
+++ mystro/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystro::Application.config.secret_key_base = '2c9d02a871650aaa72b247bb4ebb909d61f08029308e24494b6e32ea2b392a8db11f9455eac6e89372cf4690d3c316122e4c39c7d4b1755ff999861077a84b56'
