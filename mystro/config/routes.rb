7a806c04c218abc9dd3d6eacfb263a128c7020a0
Ben Lam <bklam@andrew.cmu.edu>
2014-12-23 01:18:31 -0500

    Changed routes

--- mystro/config/routes.rb
+++ mystro/config/routes.rb
@@ -1,5 +1,6 @@
 Mystro::Application.routes.draw do
-  get "home/index"
+  get 'home', to: 'home#index', as: :home
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
